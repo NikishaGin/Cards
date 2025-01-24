f645875deb9160f0dff42b8b1ef731d9ed5506c7
Nikita Sedov <nikishagin@gmail.com>
2025-01-24 15:36:46 +0300

    api2

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,19 +5,19 @@ const instance = axios.create({
     baseURL: 'http://127.0.0.1:8000/api/',
 })
 
-// Определяем типы данных
-export type CreateCardPayload = {
-    title: string;
-    description: string;
-    author: string;
-    department: string;
-    image: string;
-    file: File;
-};
+// // Определяем типы данных
+// export type CreateCardPayload = {
+//     title: string;
+//     description: string;
+//     author: string;
+//     department: string;
+//     image: string;
+//     file: File;
+// };
 
 export const cardsApi = {
-    createCards(payload: CreateCardPayload) {
-        return instance.post('upload', {payload})
+    createCards(title: string, description: string, author: string, department: string, image: string, file: File) {
+        return instance.post('upload', {title, description, author, department, image, file})
     },
 }
 
